335c7c6a6f882a8bd0ffa232a9f7c0af171ed927
Nite <solitude.loner7@gmail.com>
2020-11-01 15:58:13 +0700

    [Update] Calculator v1.0

--- script.js
+++ script.js
@@ -6,31 +6,134 @@ const multiply = (first, second) => first * second;
 
 const divide = (first, second) => second == 0 ? Infinity : first / second;
 
+const modulo = (first, second) => first % second;
+
+const sqrt = (first) => Math.sqrt(first);
+
+const invert = (first) => {
+    if ( String(first).charAt(0) == "-")
+        return Math.abs(first);
+    return -first;
+}
+
 const operate = (operator, first, second) => {
+
+    let result = 0;
+
     switch (operator) {
         case "+":
-            add(first, second);
+            result = add(first, second);
             break;
         case "-":
-            subtract(first, second);
+            result = subtract(first, second);
             break;
         case "*":
-            multiply(first, second);
+            result = multiply(first, second);
             break;
         case "/":
-            divide(first, second);
+            result = divide(first, second);
             break;
-        
+        case "%":
+            result = modulo(first, second);
+            break;
+        case "SQRT":
+            result = sqrt(first);
+            break;
+        case "+/-":
+            result = invert(first);
+            break;
+        case "=":
+            result = first ? first : second;
+            break;
     }
+
+    return Math.round(result * 10) / 10;
 }
 
-const writeToDisplay = (event) => {
-    let numberDisplay = document.querySelector("calculator__head__number");
+const showResult = (result) => {
+    let numberDisplay = document.querySelector(".calculator__head__number");
+    let operatorDisplay = document.querySelector(".calculator__head__operator");
 
-    if ( numberDisplay.textContent == 0 ) {
+    numberDisplay.textContent = result;
+    operatorDisplay.textContent = "";
+    firstNumber = result;
+    secondNumber = "";
+}
+
+const writeToNumberDisplay = (event) => {
+    let numberDisplay = document.querySelector(".calculator__head__number");
+    let operatorDisplay = document.querySelector(".calculator__head__operator");
+
+    console.dir(numberDisplay);
+
+    if ( numberDisplay.textContent == 0) {
         numberDisplay.textContent = event.target.textContent;
+    } else {
+        numberDisplay.textContent = numberDisplay.textContent + event.target.textContent;
+    }
+
+    if (typeof firstNumber == "number" && operatorDisplay.textContent) {
+        secondNumber = Number(numberDisplay.textContent);
+    } else {
+        firstNumber = Number(numberDisplay.textContent);
+    }
+
+    console.log(firstNumber, secondNumber);
+
+}
+
+const writeToOperatorDisplay = (event) => {
+    let operatorDisplay = document.querySelector(".calculator__head__operator");
+    let numberDisplay = document.querySelector(".calculator__head__number");
+
+    console.dir(operatorDisplay);
+
+    if ( operatorDisplay.textContent ) {
+        let result = operate(operatorDisplay.textContent, firstNumber, secondNumber);
+        showResult(result);
+    } else {
+        operatorDisplay.textContent = event.target.textContent;
+        if (operatorDisplay.textContent == "=" 
+            || operatorDisplay.textContent == "SQRT"
+            || operatorDisplay.textContent == "+/-"
+            ) {
+            let result = operate(operatorDisplay.textContent, firstNumber, secondNumber);
+            showResult(result);
+        } else {
+            numberDisplay.textContent = 0;
+        }
     }
 }
+
+const clearCalculator = (event) => {
+    let operatorDisplay = document.querySelector(".calculator__head__operator");
+    let numberDisplay = document.querySelector(".calculator__head__number");
+
+    firstNumber = 0;
+    secondNumber = 0;
+
+    operatorDisplay.textContent = "";
+    numberDisplay.textContent = firstNumber;
+
+}
+
+let numberButtons = document.querySelectorAll(".calculator__row__column.number");
+let operatorButtons = document.querySelectorAll(".calculator__row__column.operator");
+let clearButton = document.querySelector(".calculator__row__column.clear");
+let firstNumber = 0;
+let secondNumber;
+
+console.dir(operatorButtons);
+
+for ( let button of Array.from(numberButtons)) {
+    button.addEventListener("click", writeToNumberDisplay);
+}
+
+for ( let button of Array.from(operatorButtons)) {
+    button.addEventListener("click", writeToOperatorDisplay);
+}
+
+clearButton.addEventListener("click", clearCalculator);
 
 
 
